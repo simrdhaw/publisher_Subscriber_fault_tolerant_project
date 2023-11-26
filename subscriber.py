0fbcf44a19741324d5b093f72af35be449166cd6
Anwitha Arbi <38724750+Anwitha2806@users.noreply.github.com>
2023-11-26 00:30:18 -0800

    Update subscriber.py
    Subscriber with broker

--- subscriber.py
+++ subscriber.py
@@ -2,8 +2,62 @@ import requests
 import subprocess
 import platform
 import sys
+import boto3
+import json
+import webbrowser
 
-broker_node = '127.0.0.1:5052' 
+def get_running_aws_instances():
+    # Create an EC2 client
+    ec2 = boto3.client('ec2', region_name='us-east-2',aws_access_key_id='####', 
+                aws_secret_access_key='#####')  # Ensure your AWS credentials are configured
+    # Retrieve information about running instances
+    response = ec2.describe_instances()
+    public_ips = []
+    # Extract and display public IPv4 addresses of running instances
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            if 'PublicIpAddress' in instance:
+                public_ip = instance['PublicIpAddress']
+                security_groups = instance['SecurityGroups']
+                for sg in security_groups:
+                    sg_id = sg['GroupId']
+                    
+                    # Describe the inbound rules of the security group
+                    security_group = ec2.describe_security_groups(GroupIds=[sg_id])
+                    for rule in security_group['SecurityGroups'][0]['IpPermissions']:
+                        if 'FromPort' in rule:
+                            from_port = rule['FromPort']
+                            if from_port!=22:
+                                public_ips.append(public_ip+':'+str(from_port))
+    return public_ips
+
+def find_primary_broker():
+    public_ips = get_running_aws_instances()
+    response = requests.get(f"http://{public_ips[1]}/list_replicas")
+    response_data = response.json()  # Extract JSON data from the response
+
+    # Accessing the 'replicas' key from the JSON data
+    replicas = response_data.get('replicas', [])
+
+    # Convert JSON strings to Python dictionaries
+    replicas_data = [json.loads(replica) for replica in replicas]
+
+    # Find the broker with is_primary=True
+    primary_broker = next((json.loads(broker) for broker in replicas if json.loads(broker).get('is_primary', False)), None)
+
+    if primary_broker:
+        return primary_broker['ip']
+    else:
+        print("No primary broker found..")
+
+broker_node = find_primary_broker()
+
+#broker_node = '13.58.245.117:5052' 
+
+
+
+
+
 
 def subscribe_user():
     global broker_node
@@ -11,32 +65,33 @@ def subscribe_user():
     username = input("Enter username: ")
     topic = input("Enter topic to subscribe: ")
     try:
+        
         response = requests.post(f"http://{broker_node}/subscribe?username={username}&topic={topic}")
         print(response.json())
     except requests.RequestException:
-        
-        if broker_node == '127.0.0.1:5052': 
-            broker_node = '127.0.0.1:5053' 
-            
-        else:
-            broker_node == '127.0.0.1:5052'
+        active_nodes = get_running_aws_instances()
+        broker_node = active_nodes[0]
         print("Primary broker is down ,updating the broker the primary broker to "+broker_node)
         
         subscribe_user()
-        
 
 def stream_messages():
     username = input("Enter username to stream messages: ")
-    if sys.platform.startswith('win'):  # For Windows
-        subprocess.Popen(['start', 'cmd', '/k', 'python', 'stream_messages.py', '--username', username,'--broker_node',broker_node], shell=True)
-    elif sys.platform == 'darwin':  # For macOS
-        subprocess.Popen(['open', '-a', 'Terminal', 'python', 'stream_messages.py', '--username', username,'--broker_node',broker_node])
-    elif sys.platform.startswith('linux'):  # For Linux
-        subprocess.Popen(['x-terminal-emulator', '-e', 'python', 'stream_messages.py', '--username', username,'--broker_node',broker_node])
-    else:
-        print("Unsupported platform")
+    broker_node = find_primary_broker()
+    try:
+        print("Opening window for streaming "+username+"'s messages..")
+        webbrowser.open(f"http://{broker_node}/stream?username={username}")
+       
+    except requests.RequestException:
+        active_nodes = get_running_aws_instances()
+        broker_node = active_nodes[0]
+        print("Primary broker is down ,updating the broker the primary broker to "+broker_node)
+        stream_messages(username, broker_node)       
+
 
 def main():
+    print("Connecting to the primary broker....")
+    print("Primary broker found with ip: ",broker_node)
     while True:
         print("\nMenu:")
         print("1. Subscribe User")
@@ -51,13 +106,6 @@ def main():
             break
         else:
             print("Invalid choice. Please enter a valid option.")
-
-
-def my_stream():
-    global resp
-    resp = requests.get("http://127.0.0.1:5000/stream", params={'username': 'simran'}, stream=True)
-    for d in resp.content:
-        print(d)
 
 if __name__ == "__main__":
     print("-------------------------Organization alerts system-----------------------------")
